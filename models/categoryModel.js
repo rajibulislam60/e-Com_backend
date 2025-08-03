3e4d6c7d7d9db331b47597b1a2b5b53b20253e9f
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-08-03 23:14:48 +0600

    category model add

---
+++ models/categoryModel.js
@@ -0,0 +1,28 @@
+const { default: mongoose, Schema } = require("mongoose");
+
+let categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Category", categorySchema);
