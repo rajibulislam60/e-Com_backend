8b75bbd34c4f3c699de63ab1b9f9f71096c8e3f5
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-08-09 19:31:47 +0600

    create product api is working

--- controller/productController.js
+++ controller/productController.js
@@ -1,6 +1,44 @@
+const productModel = require("../models/productModel");
+
 const createProductsController = async (req, res) => {
   try {
-    res.send("products controller is working.");
+    const {
+      name,
+      description,
+      sellingPrice,
+      discountPrice = 0,
+      stock,
+      category,
+      isFeature = false,
+    } = req.body;
+
+    if (!name || !description || !sellingPrice || !stock || !category) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "All fields are required" });
+    }
+
+    const images =
+      req.files?.map((item) => process.env.HOST_URL + item.filename) || [];
+
+    const product = new productModel({
+      name,
+      description,
+      sellingPrice,
+      discountPrice,
+      stock,
+      category,
+      image: images,
+      isFeature,
+    });
+
+    await product.save();
+
+    res.status(200).send({
+      success: true,
+      message: "Product add successfully.",
+      data: product,
+    });
   } catch (error) {
     console.log(error);
   }
--- router/api/products.js
+++ router/api/products.js
@@ -1,9 +1,39 @@
 const express = require("express");
+const multer = require("multer");
+
 const {
   createProductsController,
 } = require("../../controller/productController");
 const router = express.Router();
 
-router.post("/createProducts", createProductsController);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const extention = file.originalname.split(".");
+    cb(
+      null,
+      file.fieldname + "-" + uniqueName + `.${extention[extention.length - 1]}`
+    );
+  },
+});
+
+function errCheck(err, req, res, next) {
+  if (err) {
+    return res.status(500).send({ success: false, msg: err.massage });
+  }
+  next();
+}
+
+const upload = multer({ storage: storage });
+
+router.post(
+  "/createProducts",
+  upload.single("image"),
+  errCheck,
+  createProductsController
+);
 
 module.exports = router;
